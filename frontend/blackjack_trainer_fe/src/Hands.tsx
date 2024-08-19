d991afe1941f3fb25ca4ed7ad1b27d44e1ff4aab
Joseph Castillo <joseph.cast0226@gmail.com>
2024-08-19 15:14:27 -0700

    Remove unused JSX element and add unique keys to list items

--- frontend/blackjack_trainer_fe/src/Hands.tsx
+++ frontend/blackjack_trainer_fe/src/Hands.tsx
@@ -5,10 +5,6 @@ interface DealerHandProps {
     faceDown: boolean;
 }
 
-interface HandProps {
-    hand: string[];
-}
-
 interface MultipleHandsProps {
     pHands: PlayerHands;
 }
@@ -17,32 +13,17 @@ export function DealerHand(props: DealerHandProps) {
     let cards: JSX.Element[] = [];
 
     if (props.faceDown) {
-        cards.push(<li>{props.hand[0]}</li>)
-        cards.push(<li>(face-down)</li>)
+        cards.push(<li key={props.hand[0]}>{props.hand[0]}</li>)
+        cards.push(<li key="facedown">(face-down)</li>)
     } else {
         cards = props.hand.map((card: string) => {
-            return <li>{card}</li>;
+            return <li key={card}>{card}</li>;
         })
     }
 
     return (
         <>
             <p>Dealer hand:</p>
-            <ul>
-                {cards}
-            </ul>
-        </>
-    )
-}
-
-export function PlayerHand(props: HandProps) {
-    const cards = props.hand.map((card: string) => {
-        return <li>{card}</li>;
-    })
-
-    return (
-        <>
-            <p>Player hand:</p>
             <ul>
                 {cards}
             </ul>
@@ -59,7 +40,7 @@ export function MultiplePlayerHands(props: MultipleHandsProps) {
         // add hand list
         const temp: JSX.Element[] = [];
         for (let j = 0; j < props.pHands.hands[i].length; j++) {
-            temp.push(<li>{props.pHands.hands[i][j]}</li>);
+            temp.push(<li key={props.pHands.hands[i][j]}>{props.pHands.hands[i][j]}</li>);
         }
         jsx.push(<ul>{temp}</ul>);
     }
